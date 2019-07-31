b725477c5143f20c3f9f6bc10458b09ac3bee5c1
shuai.zhang <shuai.zhang@wenba100.com>
2019-07-31 17:14:09 +0800

    适配器模式

---
+++ app/Structural/Adapter/EBookAdapter.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Structural\Adapter;
+
+/**
+ * 电子书适配器
+ *
+ * Class EBookAdapter
+ *
+ * @package App\Structural\Adapter
+ */
+class EBookAdapter implements BookInterface
+{
+
+    /**
+     * @var EBookInterface 电子书
+     */
+    private $eBook;
+
+    public function __construct(EBookInterface $eBook)
+    {
+        $this->eBook = $eBook;
+    }
+
+    /**
+     * 打开书
+     */
+    public function open()
+    {
+        $this->eBook->unlock();
+    }
+
+    /**
+     * 翻页
+     */
+    public function turnPage()
+    {
+        $this->eBook->pressNext();
+    }
+
+    /**
+     * 获取页数
+     *
+     * @return int
+     */
+    public function getPage(): int
+    {
+        return $this->eBook->getPage()[0];
+    }
+}
