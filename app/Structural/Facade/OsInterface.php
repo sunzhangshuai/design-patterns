f2a52ff9941da9ee726a6cdddd579c00838c2275
shuai.zhang <shuai.zhang@wenba100.com>
2019-08-01 10:54:23 +0800

    门面模式

---
+++ app/Structural/Facade/OsInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Structural\Facade;
+
+/**
+ * 创建操作系统接口类 OsInterface 。
+ */
+interface OsInterface
+{
+
+    /**
+     * 声明关机方法。
+     */
+    public function halt();
+
+    /**
+     * 声明获取名称方法，返回字符串格式数据。
+     */
+    public function getName(): string;
+}
