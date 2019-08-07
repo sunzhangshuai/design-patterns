c2d2e2f33de684b31cb515117b4c9f88abe6fba9
zhangshuai <zhangshuai@gmail.1134>
2019-08-07 15:19:55 +0800

    策略模式

---
+++ app/Behavioral/Strategy/Strategy/CashSuper.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Behavioral\Strategy\Strategy;
+
+
+abstract class CashSuper
+{
+    /**
+     * 收银
+     *
+     * @param $cash
+     *
+     * @return float
+     */
+    abstract public function acceptCash($cash): float;
+}
