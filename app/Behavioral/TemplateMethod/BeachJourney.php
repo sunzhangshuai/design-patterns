e5cba5244995f3c780ac7bb8c8c760edad5c3918
sunzhangshuai <zhangshuai1134@gmail.com>
2019-08-08 10:34:22 +0000

    Apply fixes from StyleCI

--- app/Behavioral/TemplateMethod/BeachJourney.php
+++ app/Behavioral/TemplateMethod/BeachJourney.php
@@ -1,23 +1,19 @@
 <?php
 
-
 namespace App\Behavioral\TemplateMethod;
 
 /**
- * 沙滩旅行
+ * 沙滩旅行.
  *
  * Class BeachJourney
- *
- * @package App\Behavioral\TemplateMethod
  */
 class BeachJourney extends Journey
 {
-
     /**
-     * 这个方法必须要实现，它是这个模式的关键点
+     * 这个方法必须要实现，它是这个模式的关键点.
      */
     protected function enjoyVacation(): string
     {
-        return "Swimming and sun-bathing";
+        return 'Swimming and sun-bathing';
     }
 }
