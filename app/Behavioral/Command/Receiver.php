eeafb40c1215cb1b86fa3ac0b45af267f8e6e6fd
zhangshuai <zhangshuai@gmail.1134>
2019-09-04 18:39:49 +0800

    命令模式

---
+++ app/Behavioral/Command/Receiver.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Receiver.php :
+ *
+ * PHP version 7.1
+ *
+ * @category Receiver
+ * @package  App\Behavioral\Command
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\Behavioral\Command;
+
+/**
+ * Receiver : 接收方.
+ *
+ * @category Receiver
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ * @license  http://design-patterns.test/ 设计模式
+ * @link     http://design-patterns.test/
+ */
+class Receiver
+{
+    /**
+     * 写.
+     *
+     * @param string $str
+     *
+     * @return void
+     */
+    public function write($str)
+    {
+        echo $str;
+    }
+}
