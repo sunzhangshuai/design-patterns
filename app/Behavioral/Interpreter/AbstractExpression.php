d6ab0b882c9d13ec1267ed3f74940d4dad3e7f10
zhangshuai <zhangshuai@gmail.1134>
2019-09-06 18:46:45 +0800

    解释器模式

---
+++ app/Behavioral/Interpreter/AbstractExpression.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * AbstractExpression.php :
+ *
+ * PHP version 7.1
+ *
+ * @category AbstractExpression
+ * @package  App\Behavioral\Interpreter
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\Behavioral\Interpreter;
+
+/**
+ * 抽象表达式.
+ *
+ * Interface AbstractExpression
+ *
+ * @package App\Behavioral\Interpreter
+ */
+interface AbstractExpression
+{
+    /**
+     * 解释
+     *
+     * @param Context $context
+     *
+     * @return string
+     */
+    public function interpret(Context $context);
+}
