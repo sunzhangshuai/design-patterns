378974bbce67fb2d5854017c53bf28472a2c8fa3
zhangshuai <zhangshuai@gmail.1134>
2019-09-09 14:59:15 +0800

    桥接模式

---
+++ app/Structural/Bridge/Assemble.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Assemble.php :
+ *
+ * PHP version 7.1
+ *
+ * @category Assemble
+ * @package  App\Structural\Bridge
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\Structural\Bridge;
+
+/**
+ * Assemble : 组装.
+ *
+ * @category Assemble
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+class Assemble implements Workshop
+{
+    /**
+     * 工作
+     *
+     * @return void
+     */
+    public function work()
+    {
+        echo 'Assembled';
+    }
+}
