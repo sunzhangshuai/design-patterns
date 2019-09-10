1b35bb57f708bdbce5e38451306867a5204bd550
zhangshuai <zhangshuai@gmail.1134>
2019-09-10 11:18:18 +0800

    服务提供者

---
+++ app/OtherPatterns/ServiceLocator/DatabaseService.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * DatabaseService.php :
+ *
+ * PHP version 7.1
+ *
+ * @category DatabaseService
+ * @package  App\OtherPatterns\ServiceLocator
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\OtherPatterns\ServiceLocator;
+
+/**
+ * DatabaseService : 数据库服务.
+ *
+ * @category DatabaseService
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+class DatabaseService implements DatabaseServiceInterface
+{
+
+}
