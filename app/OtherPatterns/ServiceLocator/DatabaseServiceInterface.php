a52a517373084867da87868376ffcbf7cad0ab37
zhangshuai <zhangshuai@gmail.1134>
2019-09-10 11:18:18 +0800

    服务提供者

---
+++ app/OtherPatterns/ServiceLocator/DatabaseServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * DatabaseServiceInterfaceterface.php :
+ *
+ * PHP version 7.1
+ *
+ * @category DatabaseServiceInterface
+ * @package  App\OtherPatterns\ServiceLocator
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\OtherPatterns\ServiceLocator;
+
+/**
+ * 数据库服务接口定义.
+ *
+ * Interface DatabaseServiceInterface
+ *
+ * @package App\OtherPatterns\ServiceLocator
+ */
+interface DatabaseServiceInterface
+{
+
+}
---
+++ app/OtherPatterns/ServiceLocator/LogServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * LogServiceInterface.php :
+ *
+ * PHP version 7.1
+ *
+ * @category LogServiceInterface
+ * @package  App\OtherPatterns\ServiceLocator
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\OtherPatterns\ServiceLocator;
+
+/**
+ * 日志服务接口定义
+ *
+ * Interface LogServiceInterface
+ *
+ * @package App\OtherPatterns\ServiceLocator
+ */
+interface LogServiceInterface
+{
+
+}
