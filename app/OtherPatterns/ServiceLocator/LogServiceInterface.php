f6a41f2b79a5180b066677bfcc61f4b6a7dc5881
sunzhangshuai <51902208+sunzhangshuai@users.noreply.github.com>
2019-09-10 11:19:13 +0800

    服务提供者模式

--- app/OtherPatterns/ServiceLocator/LogServiceInterface.php
+++ app/OtherPatterns/ServiceLocator/LogServiceInterface.php
@@ -1,24 +1,20 @@
 <?php
 /**
- * LogServiceInterface.php :
+ * LogServiceInterface.php :.
  *
  * PHP version 7.1
  *
  * @category LogServiceInterface
- * @package  App\OtherPatterns\ServiceLocator
  * @author   zhangshuai <zhangshaui1134@gmail.com>
  */
 
 namespace App\OtherPatterns\ServiceLocator;
 
 /**
- * 日志服务接口定义
+ * 日志服务接口定义.
  *
  * Interface LogServiceInterface
- *
- * @package App\OtherPatterns\ServiceLocator
  */
 interface LogServiceInterface
 {
-
 }
