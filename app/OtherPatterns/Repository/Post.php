c80cf663b0667f4ef8106002c734242754b0e55a
zhangshuai <zhangshuai@gmail.1134>
2019-09-10 13:35:15 +0800

    资源库模式

---
+++ app/OtherPatterns/Repository/Post.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Post.php :
+ *
+ * PHP version 7.1
+ *
+ * @category Post
+ * @package  App\OtherPatterns\Repository
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+
+namespace App\OtherPatterns\Repository;
+
+/**
+ * Post :
+ *
+ * @category Post
+ * @author   zhangshuai <zhangshaui1134@gmail.com>
+ */
+class Post
+{
+    public $id;
+    public $title;
+    public $text;
+    public $author;
+    public $created;
+}
